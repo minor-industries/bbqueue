2db9cc4ab451cfbee7ded7983c117ab7064f1bf2
suurkivi <136639517+suurkivi@users.noreply.github.com>
2024-03-12 21:22:56 -0700

    WIP: rtgraph integration

--- bbqueue.go
+++ bbqueue.go
@@ -3,16 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/minor-industries/bbqueue/database"
 	"github.com/minor-industries/bbqueue/html"
-	"github.com/minor-industries/bbqueue/plot"
 	"github.com/minor-industries/bbqueue/radio"
+	"github.com/minor-industries/rtgraph"
 	"github.com/pkg/errors"
-	"gorm.io/gorm"
-	"html/template"
-	"io/fs"
 	"os"
-	"path/filepath"
 	"strings"
 	"time"
 )
@@ -29,123 +24,41 @@ func setup() (string, error) {
 }
 
 func run() error {
-	datadir, err := setup()
+	errCh := make(chan error)
+	graph, err := rtgraph.New(os.ExpandEnv("$HOME/bbqueue.db"), errCh, []string{
+		"bbqueue_bbq01_bbq",
+		"bbqueue_bbq01_meat",
+	})
 	if err != nil {
-		return errors.Wrap(err, "setup")
+		return errors.Wrap(err, "new rtgraph")
 	}
 
-	db, err := database.Get(filepath.Join(datadir, "bbqueue.db"))
-	if err != nil {
-		return errors.Wrap(err, "get db")
-	}
-
-	go server(db)
-
-	for {
-		err := radio.Poll(func(probeName string, temp float64) error {
-			//fmt.Println("callback", probeName, temp)
-
-			result := db.Create(&database.Measurement{
-				ProbeID: probeName,
-				Time:    time.Now(),
-				Temp:    temp,
-			})
-
-			return errors.Wrap(result.Error, "create")
-		})
-
-		if err != nil {
-			fmt.Println("poll error:", err)
-		}
-	}
-}
-
-func server(db *gorm.DB) {
-	gin.SetMode(gin.ReleaseMode)
-	r := gin.Default()
-
-	templ := template.Must(template.New("").Funcs(templateFuncs).ParseFS(html.FS, "*.html"))
-	r.SetHTMLTemplate(templ)
-
-	r.GET("/", func(c *gin.Context) {
-		data, err := database.GetLatestTemps(db)
-		if err != nil {
-			_ = c.Error(err)
-			return
-		}
-
-		c.HTML(200, "index.html", map[string]any{
-			"data": data,
-			"now":  time.Now(),
-		})
-	})
-
-	r.GET("/plot.svg", func(c *gin.Context) {
-		plot.PlotHandler(db, c)
-	})
-
-	r.GET("/data.csv", func(c *gin.Context) {
-		c.Writer.Header().Set("Content-Type", "text/plain")
-		c.Status(200)
-		content, err := getCSV(db, c)
-		if err != nil {
-			c.Error(err)
-			return
-		}
-		_, _ = c.Writer.Write(content)
-	})
-
-	files(r,
-		"dygraph.js", "application/javascript",
-		"dygraph.css", "text/css",
-		"chart.html", "text/html",
+	graph.StaticFiles(html.FS,
+		"index.html", "text/html",
 	)
 
-	r.Run("0.0.0.0:8080")
-}
+	go func() {
+		gin.SetMode(gin.ReleaseMode)
+		errCh <- graph.RunServer("0.0.0.0:8076")
+	}()
 
-func getCSV(db *gorm.DB, c *gin.Context) ([]byte, error) {
-	lines := []string{
-		"Date,Temp",
-	}
+	go func() {
+		for {
+			err := radio.Poll(func(probeName string, temp float64) error {
+				//fmt.Println("callback", probeName, temp)
+				now := time.Now()
+				name := strings.ReplaceAll(probeName, "-", "_")
+				err := graph.CreateValue("bbqueue_"+name, now, temp)
+				return errors.Wrap(err, "create")
+			})
 
-	probes, err := database.GetProbes(db)
-	if err != nil {
-		return nil, errors.Wrap(err, "get probes")
-	}
-
-	fmt.Println(probes)
-
-	after, err := time.Parse("2006-01-02", "1980-01-19")
-	if err != nil {
-		panic("bad time")
-	}
-	data, err := database.GetProbeData(db, probes[1], after, time.Now())
-	for _, datum := range data {
-		lines = append(lines, fmt.Sprintf("%s,%f",
-			datum.Time.Format("2006/01/02 15:04:05"),
-			datum.Temp*9/5+32,
-		))
-	}
-
-	return []byte(strings.Join(lines, "\n")), nil
-}
-
-func files(r *gin.Engine, files ...string) {
-	for i := 0; i < len(files); i += 2 {
-		name := files[i]
-		ct := files[i+1]
-		r.GET("/"+name, func(c *gin.Context) {
-			header := c.Writer.Header()
-			header["Content-Type"] = []string{ct}
-			content, err := fs.ReadFile(html.FS, name)
 			if err != nil {
-				c.Status(404)
-				return
+				fmt.Println(errors.Wrap(err, "poll"))
 			}
-			_, _ = c.Writer.Write(content)
-		})
-	}
+		}
+	}()
+
+	return <-errCh
 }
 
 func main() {
